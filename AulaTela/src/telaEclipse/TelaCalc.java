e56159f5373a2d58ef7c7ed6b5376ab18ba08e4e
Carlos Fernandes <carlos.vinicios.ff@gmail.com>
2020-04-23 08:55:59 -0300

    implementacao da TelaCalc, fim da aula 08-04-2020

---
+++ AulaTela/src/telaEclipse/TelaCalc.java
@@ -0,0 +1,124 @@
+package telaEclipse;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaCalc {
+
+	private JFrame frame;
+	private JTextField numero1TextField;
+	private JTextField numero2TextField;
+	private JTextField resultadoTextField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCalc window = new TelaCalc();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaCalc() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		numero1TextField = new JTextField();
+		numero1TextField.setBounds(10, 101, 56, 20);
+		frame.getContentPane().add(numero1TextField);
+		numero1TextField.setColumns(10);
+
+		numero2TextField = new JTextField();
+		numero2TextField.setBounds(116, 101, 56, 20);
+		frame.getContentPane().add(numero2TextField);
+		numero2TextField.setColumns(10);
+
+		resultadoTextField = new JTextField();
+		resultadoTextField.setBounds(261, 101, 56, 20);
+		frame.getContentPane().add(resultadoTextField);
+		resultadoTextField.setColumns(10);
+
+		JLabel adicaoLabel = new JLabel("+");
+		adicaoLabel.setBounds(91, 104, 15, 14);
+		frame.getContentPane().add(adicaoLabel);
+
+		JLabel numero1Label = new JLabel("n\u00B0 1");
+		numero1Label.setBounds(20, 76, 46, 14);
+		frame.getContentPane().add(numero1Label);
+
+		JLabel resultadoLabel = new JLabel("Resultado");
+		resultadoLabel.setBounds(261, 76, 56, 14);
+		frame.getContentPane().add(resultadoLabel);
+
+		JLabel numero2Label = new JLabel("n\u00B0 2");
+		numero2Label.setBounds(126, 76, 46, 14);
+		frame.getContentPane().add(numero2Label);
+
+		JButton botaoCalculaSoma = new JButton("=");
+		botaoCalculaSoma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int numero1, numero2, resultado;
+				numero1 = Integer.parseInt(numero1TextField.getText());
+				numero2 = Integer.parseInt(numero2TextField.getText());
+				if (numero1 != 0 && numero2 != 0) {
+					resultado = numero1 + numero2;
+					resultadoTextField.setText(Integer.toString(resultado));
+				}
+
+			}
+		});
+		botaoCalculaSoma.setBounds(194, 101, 46, 20);
+		frame.getContentPane().add(botaoCalculaSoma);
+
+		JButton botaoSair = new JButton("Sair");
+		botaoSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		botaoSair.setBounds(335, 227, 89, 23);
+		frame.getContentPane().add(botaoSair);
+
+		JButton botaoLimpar = new JButton("Limpar");
+		botaoLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				numero1TextField.setText("");
+				numero2TextField.setText("");
+				resultadoTextField.setText("");
+			}
+		});
+		botaoLimpar.setBounds(335, 193, 89, 23);
+		frame.getContentPane().add(botaoLimpar);
+
+		JLabel lblNewLabel_4 = new JLabel("Aula P.O.O com Java");
+		lblNewLabel_4.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		lblNewLabel_4.setBounds(125, 11, 192, 23);
+		frame.getContentPane().add(lblNewLabel_4);
+	}
+}
